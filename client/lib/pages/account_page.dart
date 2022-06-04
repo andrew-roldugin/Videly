3bae70e1643c390518cd881235b0611882448402
PavelTolstykh <p.tolst@yandex.ru>
2022-06-05 02:47:24 +0300

    верстка страниц

--- client/lib/pages/account_page.dart
+++ client/lib/pages/account_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../widgets/video/video.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({Key? key}) : super(key: key);
@@ -12,12 +11,225 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: VideoWidget());
-    // return SafeArea(
-    //   child: Row(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //     children: [] //const [Text("Аккаунт")],
-    //   ),
-    // );
+    return SafeArea(
+      child: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                 Text(
+                  'Ваш логин: p.tolst',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight
+                          .bold), // TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                 SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  maxLines: 1,
+                  validator: (v) {},
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: const Icon(Icons.account_circle),
+                    hintText: "Название канала",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onChanged: (v) {},
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  maxLines: 5,
+                  validator: (v) {},
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: const Icon(Icons.info_rounded),
+                    hintText: "Описание канала",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onChanged: (v) {},
+                ),
+                SizedBox(
+                  //width: 10,
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (v) {},
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: const Icon(Icons.account_circle),
+                    hintText: "Пароль",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onChanged: (v) {},
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Возможность оценивания',
+                        style: TextStyle(fontSize: 16),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      ToggleSwitch(
+                        minWidth: 50.0,
+                        initialLabelIndex: 1,
+                        cornerRadius: 20.0,
+                        activeFgColor: Colors.white,
+                        inactiveBgColor: Colors.grey,
+                        inactiveFgColor: Colors.white,
+                        totalSwitches: 2,
+                        labels: ['Да', 'Нет'],
+                        // icons: [Icons.bus_alert, Icons.add_alarm],
+                        activeBgColors: [
+                          [Colors.blue],
+                          [Colors.pink]
+                        ],
+                        onToggle: (index) {
+                          print('switched to: $index');
+                        },
+                      ),
+                    ]),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Возможность комментирования',
+                        style: TextStyle(fontSize: 16),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      ToggleSwitch(
+                        minWidth: 50.0,
+                        initialLabelIndex: 1,
+                        cornerRadius: 20.0,
+                        activeFgColor: Colors.white,
+                        inactiveBgColor: Colors.grey,
+                        inactiveFgColor: Colors.white,
+                        totalSwitches: 2,
+                        labels: ['Да', 'Нет'],
+                        // icons: [Icons.bus_alert, Icons.add_alarm],
+                        activeBgColors: [
+                          [Colors.blue],
+                          [Colors.pink]
+                        ],
+                        onToggle: (index) {
+                          print('switched to: $index');
+                        },
+                      ),
+                    ]),
+
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  child: MaterialButton(
+                    height: 110,
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1.5),
+                        borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                    onPressed: () {
+                      print('+');
+                    },
+                    child: Center(
+                      child: Column(
+                        //alignment: Alignment.topCenter,
+                        children: [
+                          Text(
+                            '+',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 50),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            'Добавить фото фона',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    MaterialButton(
+                      height: 35,
+                      color: const Color.fromRGBO(65, 75, 178, 1),
+                      child: const Text(
+                        "Удалить канал",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        print("Удалить канал");
+                      },
+                    ),
+                    MaterialButton(
+                      onPressed: () {
+                        print("Выйти из аккаунта");
+                      },
+                      color: const Color.fromRGBO(65, 75, 178, 1),
+                      child: Text(
+                        "Выйти из аккаунта",
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    )
+                  ],
+                ),
+                Center(
+                  child: MaterialButton(
+                        height: 35,
+                        color: const Color.fromRGBO(65, 75, 178, 1),
+                        child: const Text(
+                          "Сохранить изменения",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          print("Сохранить изменения");
+                        },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- client/lib/pages/history_page.dart
+++ client/lib/pages/history_page.dart
@@ -10,11 +10,125 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
+    // return SafeArea(
+    //   child: Row(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: const [Text("История просмотров")],
+    //   ),
+    // );
     return SafeArea(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [Text("История просмотров")],
+      child: Column(
+        children: [
+          SizedBox(height: 10),
+          Container(
+            child: Text(
+              'История просмотров',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: _VideoList(),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _VideoList extends StatelessWidget {
+  const _VideoList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.only(bottom: 50),
+      itemCount: 10,
+      itemExtent: 150,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black.withOpacity(0.2)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 8,
+                      offset: Offset(0, 2),
+                    )
+                  ],
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Image.asset("assets/images/no_video_preview.png",
+                          width: 80, height: 80),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Название видео',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "15:00 04.06.2022",
+                          style: TextStyle(color: Colors.grey),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          //"Описание тестового видео",
+                          'dkladsmsalkdmklasmklcmwlekcndqwmimeclcsjlqneidcqwxm csaxml kdsmklaqwcl,,',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    )),
+                    SizedBox(
+                      width: 10,
+                    ),
+                  ],
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  onTap: () {
+                    print(111);
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
